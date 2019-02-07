a93b130d4c81d74d80a9ca1bd133150ada702fd8
mac40 <marcobgg4@gmail.com>
2019-02-07 16:49:51 +0100

    deltaQ/deltaR

--- Project/heston_model.py
+++ Project/heston_model.py
@@ -12,6 +12,7 @@ def nmle_cekf(S):
     Normal Maximium Likelihood Estimation Consistent Extended Kalman Filter function
     '''
     # VARIABLES
+    S = np.append(np.array([np.mean(S[:5])]), S) # new S[0]
 
     #   EXTENDED KALMAN FILTER
     V_HAT = np.zeros(len(S)) # Predicted Volatility
@@ -36,20 +37,22 @@ def nmle_cekf(S):
 
     # AUXILIARY VARIABLES
     ZETA = np.zeros(len(S))
-    for i in range(0, len(S)-1):
-        ZETA[i] = np.log(S[i+1]) - np.log(S[i])
     deltaT = 1/250 # year/measurements_per_year ration
     R = 0.0005 # Annual interest rate
-    deltaQ = np.zeros(len(S)) # tbd
-    deltaR = np.zeros(len(S)) # tbd
+    deltaQ = np.zeros(len(S)) # Consistency variable
+    deltaR = np.zeros(len(S)) # Consistency variable
     deltaW1 = np.zeros(len(S))
     deltaW2 = np.zeros(len(S))
     delta = 0.01
 
     # INITIALIZATION
-    S = np.append(np.array([np.mean(S[:5])]), S) # new S[0]
     V_HAT[0] = np.var(np.log(S[1:5]/S[:4]))
     P[0] = np.mean((S[:5]-np.mean(S[:5]))**4)
+
+    for i in range(0, len(S)-1):
+        ZETA[i] = np.log(S[i+1]) - np.log(S[i])
+    for i in range(0, len(S)-1):
+        Q[i] = np.diag(np.array([1, 1]))
 
     KAPTH_HAT[0] = 1
     THETA_HAT[0] = 0.250
@@ -57,7 +60,7 @@ def nmle_cekf(S):
     RO_HAT[0] = 0.0001
 
     #UPDATE
-    for k, _ in enumerate(S):
+    for k in range(0, len(S)-1):
         # LINEARIZATION MATRICES OF THE STATE FUNCTION
         F[k] = 1 - KAPTH_HAT[k] * deltaT
         L[k] = np.array([0, SIGMA_HAT[k] * np.sqrt(V_HAT[k] * deltaT)])
@@ -65,6 +68,11 @@ def nmle_cekf(S):
         V_BAR[k+1] = V_HAT[k] \
                     + KAPTH_HAT[k] * SIGMA_HAT[k] * deltaT \
                     - KAPTH_HAT[k] * V_HAT[k] * deltaT
+        deltaQ[k] = P[k] * max(np.absolute(1 - KAPTH_HAT[:k+1] * deltaT)**2) \
+                    + deltaT**2 * max(np.absolute(KAPTH_HAT[:k+1] * THETA_HAT[:k+1])**2) \
+                    + max(np.absolute(SIGMA_HAT[k])**2) * deltaT * V_HAT[k] * Q[k][1, 1] \
+                    - F[k] * P[k] * np.transpose(F[k]) \
+                    + L[k] * Q[k] * np.transpose(L[k])
         P_BAR[k+1] = F[k] * P[k] * np.transpose(F[k]) \
                     + L[k] * Q[k] * np.transpose(L[k]) \
                     + deltaQ[k]
@@ -81,6 +89,11 @@ def nmle_cekf(S):
                   + H[k+1] * L[k] * Q[k] * np.transpose(M[k+1]) \
                   + M[k+1] * Q[k] * np.transpose(L[k]) * np.transpose(H[k+1]))**(-1)
         V_HAT[k+1] = V_BAR[k+1] + K[k+1] * (ZETA[k+1] - 1/2 * V_BAR[k+1])
+        deltaR[k+1] = P_BAR[k+1] * (1 + (K[k+1] * deltaT)/2)**2 \
+                     + 2 * K[k+1]**2 * deltaT * V_BAR[k] * ((1 - RO_HAT[k]) * Q[k][0, 0] \
+                     + RO_HAT[k]**2 * Q[k][1, 1]) \
+                     - P_BAR[k+1] \
+                     + K[k+1] * (H[k+1] * P_BAR[k+1] + M[k+1] * np.transpose(L[k]))
         P[k+1] = P_BAR[k+1] \
                 - K[k+1] \
                 * (H[k+1] * P_BAR[k+1] + M[K+1] * np.transpose(L[k])) \
