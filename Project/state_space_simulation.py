a3f1cd4af0beb0bd6b6595a5f024b32575124543
mac40 <marcobgg4@gmail.com>
2019-02-12 11:17:42 +0100

    simulation discretization method 1

--- Project/state_space_simulation.py
+++ Project/state_space_simulation.py
@@ -30,12 +30,15 @@ def state_space_heston_discrete(param, prev_obs, delta_motions):
     sigma = param["sigma"]
     delta = param["delta"]
     rho = param["rho"]
-    #delta_W_v = delta_motions[0]
-    #delta_W_s = delta_motions[1]
-    V = prev_obs + k * (theta - prev_obs) * delta + sigma * np.sqrt(prev_obs) * np.sqrt(delta) * np.random.normal()
-    V = max((V,0))
-    z = (r - V/2) * delta + np.sqrt((1 - rho**2)*V) * np.sqrt(delta) * np.random.normal() + rho * np.sqrt(V) * np.sqrt(delta) * np.random.normal()
-    return z, V
+    # delta_W_v = delta_motions[0]
+    # delta_W_s = delta_motions[1]
+    V_tilde = (prev_obs + k * (theta - max((prev_obs, 0))) * delta
+               + sigma * np.sqrt(max((prev_obs, 0))) * np.sqrt(delta) * np.random.normal())
+    V = max((V_tilde, 0))
+    z = ((r - V/2) * delta
+         + np.sqrt((1 - rho**2)*V) * np.sqrt(delta) * np.random.normal()
+         + rho * np.sqrt(V) * np.sqrt(delta) * np.random.normal())
+    return z, V, V_tilde
 
 
 def generate_stock_data(n, param, motions):
@@ -48,13 +51,16 @@ def generate_stock_data(n, param, motions):
     delta_W_s = np.diff(W_s)
     z = np.empty(n + 1)
     V = np.empty(n + 1)
+    V_tilde = np.empty(n + 1)
     S = np.empty(n + 2)
     V[0] = 0.2
+    V_tilde[0] = 0.2
     S[1] = 100
     for k in range(1, n+1):
-        s = state_space_heston_discrete(param, V[k-1], [delta_W_v[k-1], delta_W_s[k-1]])
+        s = state_space_heston_discrete(param, V_tilde[k-1], [delta_W_v[k-1], delta_W_s[k-1]])
         z[k] = s[0]
         V[k] = s[1]
+        V_tilde[k] = s[2]
         S[k+1] = S[k] * np.exp(z[k])
     return S, z, V
 
