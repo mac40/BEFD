db9bb69a1878ea40077d068f660d1a2c96dec343
mac40 <marcobgg4@gmail.com>
2019-02-12 11:21:59 +0100

    simulation cleanup

--- Project/state_space_simulation.py
+++ Project/state_space_simulation.py
@@ -6,21 +6,9 @@ Model simulation from Heston model state space
 
 
 import numpy as np
-import pandas as pd
 
 
-def generate_brownian_motion(n, delta_t=1):
-    '''
-    Function to generate brownian motion
-    '''
-    B = np.empty(n+1)
-    B[0] = 0
-    for i in range(1, n+1):
-        B[i] = np.random.normal(0, delta_t) + B[i-1]
-    return B
-
-
-def state_space_heston_discrete(param, prev_obs, delta_motions):
+def state_space_heston_discrete(param, prev_obs):
     '''
     State space heston discrete (need better definition)
     '''
@@ -41,14 +29,10 @@ def state_space_heston_discrete(param, prev_obs, delta_motions):
     return z, V, V_tilde
 
 
-def generate_stock_data(n, param, motions):
+def generate_stock_data(n, param):
     '''
     Generating stock data
     '''
-    W_v = motions.W_v
-    W_s = motions.W_s
-    delta_W_v = np.diff(W_v)
-    delta_W_s = np.diff(W_s)
     z = np.empty(n + 1)
     V = np.empty(n + 1)
     V_tilde = np.empty(n + 1)
@@ -57,7 +41,7 @@ def generate_stock_data(n, param, motions):
     V_tilde[0] = 0.2
     S[1] = 100
     for k in range(1, n+1):
-        s = state_space_heston_discrete(param, V_tilde[k-1], [delta_W_v[k-1], delta_W_s[k-1]])
+        s = state_space_heston_discrete(param, V_tilde[k-1])
         z[k] = s[0]
         V[k] = s[1]
         V_tilde[k] = s[2]
@@ -67,8 +51,5 @@ def generate_stock_data(n, param, motions):
 
 if __name__ == '__main__':
     n = 1000
-    W_v = generate_brownian_motion(n).reshape(n+1, 1)
-    W_s = generate_brownian_motion(n).reshape(n+1, 1)
-    motions = pd.DataFrame(np.concatenate([W_v, W_s], axis=1), columns=["W_v", "W_s"])
     param = {"r" : 0.005, "k" : 1, "theta" : 0.25, "sigma" : 0.5, "delta" : 0.01, "rho" : 0.0001}
-    S, z, V = generate_stock_data(n, param, motions)
+    S, z, V = generate_stock_data(n, param)
