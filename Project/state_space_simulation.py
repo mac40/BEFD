c82dab772b72a5ab198e7927f2a5399b6ad4056e
dparolo <34452998+dparolo@users.noreply.github.com>
2019-02-14 13:03:16 +0100

    added nmle discretization

--- Project/state_space_simulation.py
+++ Project/state_space_simulation.py
@@ -9,9 +9,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def state_space_heston_discrete(param, prev_obs):
+def state_space_heston_discrete_euler(param, prev_obs):
     '''
-    Volatility simulation
+    Volatility simulation with euler discretisation
     '''
     r = param["r"]
     k = param["k"]
@@ -28,8 +28,31 @@ def state_space_heston_discrete(param, prev_obs):
          + rho * np.sqrt(V) * np.sqrt(delta) * epsilons[1])
     return z, V, V_tilde
 
+def state_space_heston_discrete_nmle(param, prev_obs):
+    '''
+    Volatility simulation with euler discretisation
+    '''
+    r = param["r"]
+    k = param["k"]
+    theta = param["theta"]
+    sigma = param["sigma"]
+    delta = param["delta"]
+    rho = param["rho"]
+    epsilons = [np.random.normal(), np.random.normal()]
+    eta = 0.0001
+    V_tilde_sq = np.sqrt(prev_obs) 
+    + 1/(2 * np.sqrt(np.max(prev_obs,eta))) * (k * theta - k * prev_obs - 1/4 * sigma ** 2) * delta 
+    + 1/2 * sigma * np.sqrt(delta) * epsilons[1]
+    V_sq = max((V_tilde_sq, 0))
+    V = V_sq ** 2
+    V_tilde = V_tilde_sq ** 2
+    z = ((r - V/2) * delta
+         + V_sq * np.sqrt(1 - rho**2) * np.sqrt(delta) * epsilons[0]
+         + rho * V_sq * np.sqrt(delta) * epsilons[1])
+    return z, V, V_tilde
 
-def generate_stock_data(n, param):
+
+def generate_stock_data(n, param, discretization):
     '''
     Generating stock data
     '''
@@ -41,7 +64,10 @@ def generate_stock_data(n, param):
     V_tilde[0] = 0.2
     S[1] = 100
     for k in range(1, n+1):
-        s = state_space_heston_discrete(param, V_tilde[k-1])
+        if discretization == "euler":
+            s = state_space_heston_discrete_euler(param, V_tilde[k-1])
+        if discretization == "nmle":
+            s = state_space_heston_discrete_euler(param, V_tilde[k-1])
         z[k] = s[0]
         V[k] = s[1]
         V_tilde[k] = s[2]
@@ -52,6 +78,6 @@ def generate_stock_data(n, param):
 if __name__ == '__main__':
     n = 1000
     param = {"r" : 0.005, "k" : 1, "theta" : 0.25, "sigma" : 0.5, "delta" : 0.01, "rho" : 0.0001}
-    S, z, V = generate_stock_data(n, param)
+    S, z, V = generate_stock_data(n, param, "nmle")
     plt.plot(S)
     plt.show()
