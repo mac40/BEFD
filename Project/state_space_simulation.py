4a0f3637be7c0fdfec25c1bb137ca29a77cbebdf
mac40 <marcobgg4@gmail.com>
2019-02-18 11:47:05 +0100

    refactor

--- Project/state_space_simulation.py
+++ Project/state_space_simulation.py
@@ -107,41 +107,44 @@ def simulation(n, param):
         S[i] = S[i-1] * np.exp(z[i])
     return S, z, V, sqrt_V
 
-def nmle(V_HAT,delta,R):
-    
+def nmle(V_HAT, delta, R):
+    '''
+    parameter estimation from simulated volatilities
+    '''
+
     T = len(V_HAT)
     deltaW1 = np.zeros(T)
     deltaW2 = np.zeros(T)
-    
+
     P_HAT = ((1/T * np.sum(np.sqrt(V_HAT[:T-1] * V_HAT[1:T]))
-                  - 1/(T**2) * np.sum(np.sqrt(V_HAT[1:T]/V_HAT[:T-1])) * np.sum(V_HAT[:T-1]))
-                 / (delta/2 - delta/2 * 1/(T**2) * np.sum(1/V_HAT[:T-1]) * np.sum(V_HAT[:T-1])))
+              - 1/(T**2) * np.sum(np.sqrt(V_HAT[1:T]/V_HAT[:T-1])) * np.sum(V_HAT[:T-1]))
+             / (delta/2 - delta/2 * 1/(T**2) * np.sum(1/V_HAT[:T-1]) * np.sum(V_HAT[:T-1])))
 
     # KAPH THETA SIGMA GENERATION
-    KAPTH_HAT = (2/delta * (1 + (P_HAT * delta)/2 * 1/T * np.sum(1/V_HAT[:T-1]) 
-                        - 1/T * np.sum(np.sqrt(V_HAT[1:T]/V_HAT[:T-1]))))
-    
+    KAPTH_HAT = (2/delta * (1 + (P_HAT * delta)/2 * 1/T * np.sum(1/V_HAT[:T-1])
+                            - 1/T * np.sum(np.sqrt(V_HAT[1:T]/V_HAT[:T-1]))))
+
     SIGMA_HAT = np.sqrt(4/delta * 1/T
-                             * np.sum((np.sqrt(V_HAT[1:T])
-                                       - np.sqrt(V_HAT[:T-1])
-                                       - delta/(2 * np.sqrt(V_HAT[:T-1]))
-                                       * (P_HAT - KAPTH_HAT * V_HAT[:T-1]))**2))
+                        * np.sum((np.sqrt(V_HAT[1:T])
+                                  - np.sqrt(V_HAT[:T-1])
+                                  - delta/(2 * np.sqrt(V_HAT[:T-1]))
+                                  * (P_HAT - KAPTH_HAT * V_HAT[:T-1]))**2))
     THETA_HAT = (P_HAT + 1/4 * SIGMA_HAT**2)/KAPTH_HAT
-    
+
     for k in range(0, T-1):
-            deltaW1[k+1] = (np.log(S[k+1]) - np.log(S[k]) -
-                            (R - 1/2 * V_HAT[k]) * delta) / (np.sqrt(V_HAT[k]))
-            deltaW2[k+1] = (V_HAT[k+1]
-                            - V_HAT[k]
-                            - KAPTH_HAT
-                            * (THETA_HAT - V_HAT[k])
-                            * delta) / (SIGMA_HAT * np.sqrt(V_HAT[k]))
+        deltaW1[k+1] = (np.log(S[k+1]) - np.log(S[k]) -
+                        (R - 1/2 * V_HAT[k]) * delta) / (np.sqrt(V_HAT[k]))
+        deltaW2[k+1] = (V_HAT[k+1]
+                        - V_HAT[k]
+                        - KAPTH_HAT
+                        * (THETA_HAT - V_HAT[k])
+                        * delta) / (SIGMA_HAT * np.sqrt(V_HAT[k]))
 
     # RHO GENERATION
     RO_HAT = 1/(T * delta) * np.sum(deltaW1[1:T] * deltaW2[1:T])
-    
-    return KAPTH_HAT, SIGMA_HAT,THETA_HAT,RO_HAT
-        
+
+    return KAPTH_HAT, SIGMA_HAT, THETA_HAT, RO_HAT
+
 
 if __name__ == '__main__':
     # initialization parameters
@@ -150,7 +153,7 @@ if __name__ == '__main__':
              "sigma": 0.5, "delta": 0.01, "rho": 0.001}
 
     # simulation
-    S, z, V = generate_stock_data(n, param,'nmle')
+    S, z, V = generate_stock_data(n, param, 'nmle')
 
     # Plotting generated data
     fig, axes = plt.subplots(2, 2)
@@ -160,10 +163,8 @@ if __name__ == '__main__':
     axes[0, 1].set_title('z')
     axes[1, 0].plot(V)
     axes[1, 0].set_title('V')
-    """    axes[1, 1].plot(sqrt_V)
-    axes[1, 1].set_title('sqrt_V')"""
     plt.subplots_adjust(wspace=0.4, hspace=0.4)
     plt.show()
-    
-    KAPTH_HAT, SIGMA_HAT,THETA_HAT,RO_HAT = nmle(V,delta = 0.01, R = 0.005)
-    print(KAPTH_HAT, SIGMA_HAT,THETA_HAT,RO_HAT)
+
+    KAPTH_HAT, SIGMA_HAT, THETA_HAT, RO_HAT = nmle(V, delta=0.01, R=0.005)
+    print(KAPTH_HAT, SIGMA_HAT, THETA_HAT, RO_HAT)
